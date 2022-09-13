dbe4edfbb496cdb34ef37b78cfa3b1a4594a3c96
Olena <lodki-omega@ukr.net>
2022-09-13 22:40:22 +0200

    frst

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,30 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const bodyEl = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+
+startBtn.addEventListener('click', onClickStartColor);
+stopBtn.addEventListener('click', onClickStopColor);
+
+let timerId = null;
+
+function onClickStartColor() {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+
+    timerId = setInterval(() => {
+        colorRandom = getRandomHexColor();
+        bodyEl.style.background = colorRandom;
+    }, 1000);
+}
+
+function onClickStopColor() {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+
+    clearInterval(timerId);
+}
---
+++ src/js/03-promises.js
@@ -0,0 +1,42 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(e) {
+  e.preventDefault();
+
+  let delay = Number(e.currentTarget.delay.value);
+  const step = Number(e.currentTarget.step.value);
+  const amount = Number(e.currentTarget.amount.value);
+
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, { useIcon: false });
+        }, delay);
+      })
+      .catch(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, { useIcon: false });
+        }, delay);
+      });
+    delay += step;
+  }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    const objectPromise = { position, delay };
+  
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(objectPromise);
+      } else {
+        reject(objectPromise);
+      }
+    });
+  })
+}  
