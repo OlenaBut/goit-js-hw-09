ac93f8cbcbe52cc9de6aaa932e3d61e9632f2ce1
OlenaBut <99390346+OlenaBut@users.noreply.github.com>
2022-09-13 20:44:40 +0000

    Deploying to gh-pages from @ OlenaBut/goit-js-hw-09@dbe4edfbb496cdb34ef37b78cfa3b1a4594a3c96 🚀

---
+++ 01-color-switcher.10b9b3ab.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("body"),t=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]");t.addEventListener("click",(function(){t.disabled=!0,n.disabled=!1,o=setInterval((function(){colorRandom="#".concat(Math.floor(16777215*Math.random()).toString(16)),e.style.background=colorRandom}),1e3)})),n.addEventListener("click",(function(){t.disabled=!1,n.disabled=!0,clearInterval(o)}));var o=null}();
+//# sourceMappingURL=01-color-switcher.10b9b3ab.js.map
