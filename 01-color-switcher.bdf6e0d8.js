8cc1fb3be0a29c59accb048b73838521d914ef71
OlenaBut <99390346+OlenaBut@users.noreply.github.com>
2022-09-17 15:30:12 +0000

    Deploying to gh-pages from @ OlenaBut/goit-js-hw-09@abd4342696153d145f7eed0f0315a72536c976e0 🚀

--- 01-color-switcher.bdf6e0d8.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("body"),t=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");t.addEventListener("click",(function(){t.disabled=!0,d.disabled=!1,o=setInterval((()=>{colorRandom=`#${Math.floor(16777215*Math.random()).toString(16)}`,e.style.background=colorRandom}),1e3)})),d.addEventListener("click",(function(){t.disabled=!1,d.disabled=!0,clearInterval(o)}));let o=null;
-//# sourceMappingURL=01-color-switcher.bdf6e0d8.js.map
